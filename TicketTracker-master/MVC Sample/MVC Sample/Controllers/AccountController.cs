07b1544783a0364337eb81aa91d8432886ace641
sateesh.ganaparapu <sateesh.ganaparapu@excelra.com>
2018-04-23 11:53:30 +0530

    Code added for angular auth metthod.

--- TicketTracker-master/MVC Sample/MVC Sample/Controllers/AccountController.cs
+++ TicketTracker-master/MVC Sample/MVC Sample/Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace MVC_Sample.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -34,9 +34,9 @@ namespace MVC_Sample.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -120,7 +120,7 @@ namespace MVC_Sample.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -155,8 +155,8 @@ namespace MVC_Sample.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
@@ -401,6 +401,18 @@ namespace MVC_Sample.Controllers
         public ActionResult ExternalLoginFailure()
         {
             return View();
+        }
+
+        public ActionResult checkUser(string userName, string password)
+        {
+            if (userName == "admin" & password == "admin@123")
+            {
+                return Json(new { status = "success", msg = "Successfully login." });
+            }
+            else
+            {
+                return Json(new { status = "error", msg = "Invalid details." });
+            }
         }
 
         protected override void Dispose(bool disposing)
